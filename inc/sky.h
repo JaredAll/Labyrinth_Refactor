a4e61503803341be368c201cdd49355ca142aff1
Jared Allen <jla4782@truman.edu>
2020-03-02 17:30:03 -0600

    sky resources

---
+++ inc/sky.h
@@ -0,0 +1,12 @@
+#include "scenery_config.h"
+
+int sky_x = 0;
+int sky_y = 0;
+
+std::string sky_resource = "/home/jared/Games/Labyrinth/resources/sprites/sky.png";
+
+uint sky_repititions = 5;
+
+SceneryConfig sky_config = {
+  sky_x, sky_y, sky_resource, sky_repititions
+};
